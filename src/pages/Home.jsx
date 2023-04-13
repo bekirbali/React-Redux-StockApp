1ca38f5c57daab2fc42cc3c7c2378ea71c4b58ff
bekirbali <bfbali43@gmail.com>
2023-04-14 02:55:52 +0300

    Kpi Chart Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import KpiCards from "../components/KpiCards";
+import Charts from "../components/Charts";
+import { useEffect } from "react";
+import useStockCall from "../hooks/useStockCall";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { getStockData } = useStockCall();
+
+  useEffect(() => {
+    getStockData("sales");
+    getStockData("purchases");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+      <KpiCards />
+      <Charts />
+    </div>
+  );
 };
 
 export default Home;
